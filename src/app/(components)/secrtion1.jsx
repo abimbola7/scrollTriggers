2de9db730959f96d81ec25f07f1fbaaf1715add8
abimbola7 <abimbolababajide6@gmail.com>
2025-05-06 05:55:00 +0100

    commit

--- src/app/(components)/secrtion1.jsx
+++ src/app/(components)/secrtion1.jsx
@@ -9,23 +9,32 @@ import { SplitText } from 'gsap/SplitText'
 gsap.registerPlugin(TextPlugin, SplitText)
 
 const Section1 = () => {
+  const timeline = React.useRef()
   const { context, contextSafe } = useGSAP(() => {
     let split = SplitText.create(".split", {
       type : "words, chars",
     })
-
-    gsap.from(split.chars, {
+    
+    timeline.current = gsap.timeline({})
+    .from(split.chars, {
       duration : 1,
       y : 100,
       autoAlpha : 1,
       stagger : .05
-    })
+    }).to(".text-1", {
+      delay : .5,
+      scale : 70,
+      transformOrigin : "center",
+      duration : 2,
+      ease : "power4.in "
+    }
+    )
 
     console.log(split)
   })
   return (
     <div className='w-full h-screen bg-white flex items-center justify-center text-black'>
-      <div className='w-full max-w-[300px] text-5xl'>
+      <div className='w-full max-w-[300px] text-5xl text-1'>
         <p className='text-left split overflow-hidden font-medium'>Oladosu</p>
         <p className='text-right split overflow-hidden font-medium'>Abimbola</p>
       </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white overflow-hidden`}
       >
         <MouseProvider>
           {children}
